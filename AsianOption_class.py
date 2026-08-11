2463a71a4e5d69d06d33fa45bade1b09cab44126
Nicola Bartolini <bart1995olo@gmail.com>
2026-08-11 10:57:07 +0200

    create Asian Option Class. File AsianOption_class.py

---
+++ AsianOption_class.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 09:24:24 2026
+
+@author: Nicola
+"""
+
+from abc import ABC, abstractmethod, abstractproperty
+from datetime import datetime
+from scipy.stats.mstats import gmean
+import numpy as np 
+
+
+##################################
+
+class AsianOption(ABC):
+    
+    @abstractmethod 
+    def set_market_premium(self, P : float) -> None:
+        pass 
+    
+    @abstractproperty 
+    def get_premium(self) -> float :
+        pass
+    
+    @abstractmethod 
+    def get_tenor(self, date : datetime):
+        pass 
+    
+    @abstractmethod 
+    def payoff(self, S : np.array):
+        pass
+    
+
+class AsianCall(AsianOption):
+    
+    def __init__(self, strike : float, maturity_date : datetime, method : str):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        
+        self.market_premium = np.nan 
+        self.method = method
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+        
+    def get_premium(self) -> float :
+        return self.market_premium
+    
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def payoff(self, S):
+        
+        if self.method.lower()=='arithmetic':
+        
+            ST = np.mean(S, axis=0)
+        
+        elif self.method.lower() == 'geometric':
+            
+            ST = gmean(S, axis=0)
+        
+        else:
+            raise ValueError('Only arithmetic or geometric average')
+        
+        return np.maximum(ST-self.strike, 0)
+
+
+class AsianPut(AsianOption):
+    
+    def __init__(self, strike : float, maturity_date : datetime, method : str):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        
+        self.market_premium = np.nan 
+        self.method = method
+
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+        
+    def get_premium(self) -> float :
+        return self.market_premium
+    
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def payoff(self, S):
+        
+        if self.method.lower()=='arithmetic':
+        
+            ST = np.mean(S, axis=0)
+        
+        elif self.method.lower() == 'geometric':
+            
+            ST = gmean(S, axis=0)
+        
+        else:
+            raise ValueError('Only arithmetic or geometric average')
+        
+        return np.maximum(self.strike - ST, 0)
+        
+        
+        
+        
