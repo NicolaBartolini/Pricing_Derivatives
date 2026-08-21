84618f92d2558fc5192e14b16ba55abf01cce93e
Nicola Bartolini <bart1995olo@gmail.com>
2026-08-21 15:45:03 +0200

    more barrier option

--- BarrierOption_class.py
+++ BarrierOption_class.py
@@ -214,4 +214,160 @@ class UpOutCall(BarrierOption):
         
         return np.maximum(S[-1] * (1-activation) - self.strike, 0)
     
+
+############## put options 
+
+class DownInPut(BarrierOption):
     
+    def __init__(self, strike : float, barrier : float, maturity_date):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        self.barrier = barrier
+        
+        self.market_premium = np.nan 
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+        
+    def get_premium(self) -> float :
+        return self.market_premium 
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date 
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def get_barrier(self) -> float :
+        return self.barrier
+    
+    def payoff(self, S, nColumns):
+        
+        activation = np.zeros(nColumns)
+        
+        for i in range(nColumns):
+            
+            activation[i] = check_lower_barrier_touch(S[:,i], self.barrier)
+        
+        return np.maximum(self.strike - S[-1] * activation, 0)
+
+
+
+class DownOutPut(BarrierOption):
+    
+    def __init__(self, strike : float, barrier : float, maturity_date):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        self.barrier = barrier
+        
+        self.market_premium = np.nan 
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+    
+    def get_barrier(self) -> float :
+        return self.barrier
+        
+    def get_premium(self) -> float :
+        return self.market_premium 
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date 
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def payoff(self, S, nColumns):
+        
+        activation = np.zeros(nColumns)
+        
+        for i in range(nColumns):
+            
+            activation[i] = check_lower_barrier_touch(S[:,i], self.barrier)
+        
+        return np.maximum(self.strike - S[-1] * (1-activation), 0)
+
+
+class UpInPut(BarrierOption):
+    
+    def __init__(self, strike : float, barrier : float, maturity_date):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        self.barrier = barrier
+        
+        self.market_premium = np.nan 
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+        
+    def get_premium(self) -> float :
+        return self.market_premium 
+    
+    def get_barrier(self) -> float :
+        return self.barrier
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date 
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def payoff(self, S, nColumns):
+        
+        activation = np.zeros(nColumns)
+        
+        for i in range(nColumns):
+            
+            activation[i] = check_upper_barrier_touch(S[:,i], self.barrier)
+        
+        return np.maximum(self.strike - S[-1] * activation, 0)
+
+class UpOutPut(BarrierOption):
+    
+    def __init__(self, strike : float, barrier : float, maturity_date):
+        
+        self.strike = strike 
+        self.maturity_date = maturity_date 
+        self.barrier = barrier
+        
+        self.market_premium = np.nan 
+        
+    def set_market_premium(self, P):
+        
+        self.market_premium = P 
+        
+    def get_premium(self) -> float :
+        return self.market_premium 
+    
+    def get_tenor(self, date):
+        
+        diff = self.maturity_date - date 
+        
+        tenor = diff.days/365 
+        
+        return tenor  
+    
+    def get_barrier(self) -> float :
+        return self.barrier
+    
+    def payoff(self, S, nColumns):
+        
+        activation = np.zeros(nColumns)
+        
+        for i in range(nColumns):
+            
+            activation[i] = check_upper_barrier_touch(S[:,i], self.barrier)
+        
+        return np.maximum(self.strike - S[-1] * (1-activation), 0)
